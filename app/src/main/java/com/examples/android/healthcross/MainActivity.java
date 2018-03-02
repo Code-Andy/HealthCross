e08a09909b787b7d2d2b01a7c694987673f7c51f
Andy Zhang <zhangandy915@gmail.com>
2018-03-02 11:39:11 -0500

    update.166666

--- app/src/main/java/com/examples/android/healthcross/MainActivity.java
+++ app/src/main/java/com/examples/android/healthcross/MainActivity.java
@@ -1,13 +1,12 @@
 package com.examples.android.healthcross;
 
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +22,24 @@ public class MainActivity extends AppCompatActivity {
         mToggle =  new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open,R.string.close);
         mDrawerLayout.addDrawerListener(mToggle);
         mToggle.syncState();
+
+        NavigationView navigationView = (NavigationView)findViewById(R.id.navview);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        // set item as selected to persist highlight
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
@@ -51,27 +68,6 @@ public class MainActivity extends AppCompatActivity {
          return super.onOptionsItemSelected(item);
      }
 
-
-
-    public void openTrackerView(View view) {
-        Intent t = new Intent(this, TrackerActivity.class);
-        startActivity(t);
-    }
-
-    public void openNewsView(View view) {
-        Intent n = new Intent(this, NewsActivity.class);
-        startActivity(n);
-    }
-
-    public void openSettingsView(View view) {
-        Intent s = new Intent(this, SettingsActivity.class);
-        startActivity(s);
-    }
-
-    public void openInfoView(View view) {
-        Intent i = new Intent(this, InfoActivity.class);
-        startActivity(i);
-    }
 
 
 }
